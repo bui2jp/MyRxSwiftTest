fd98200ddb2a9ff8f86b1956d4c3730740118f1e
Takuya <takuya@okuyama-mac.local>
2020-02-16 16:31:27 +0900

    add rx test

---
+++ MyRxSwiftTest/MyRxSwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRxSwiftTest
+//
+//  Created by 奥山拓弥 on 2020/02/16.
+//  Copyright © 2020 奥山拓弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
